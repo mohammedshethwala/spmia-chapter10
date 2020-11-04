80eacd3b3ac362885e0af41593d6f741ae5c1341
Mohammed Shethwala <mohammedshethwala@users.noreply.github.com>
2020-11-03 18:56:39 -0600

    Updating to Springboot 2

--- organization-service/src/main/java/com/thoughtmechanix/organization/controllers/OrganizationServiceController.java
+++ organization-service/src/main/java/com/thoughtmechanix/organization/controllers/OrganizationServiceController.java
@@ -5,6 +5,9 @@ import com.thoughtmechanix.organization.events.source.SimpleSourceBean;
 import com.thoughtmechanix.organization.model.Organization;
 import com.thoughtmechanix.organization.services.OrganizationService;
 import com.thoughtmechanix.organization.utils.UserContext;
+
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +31,9 @@ public class OrganizationServiceController {
     public Organization getOrganization( @PathVariable("organizationId") String organizationId) {
         logger.debug("Looking up data for org {}", organizationId);
 
-        Organization org = orgService.getOrg(organizationId);
-        org.setContactName(org.getContactName());
-        return org;
+        Optional<Organization> org = orgService.getOrg(organizationId);
+        org.get().setContactName(org.get().getContactName());
+        return org.get();
     }
 
     @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
